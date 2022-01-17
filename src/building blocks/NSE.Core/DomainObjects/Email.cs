7ce5c2eac3bcbda1c273d0278d08b4b90ba03981
brunoverc <bruno.nv@hotmail.com>
2022-01-17 20:21:55 +0000

    Catalog

---
+++ src/building blocks/NSE.Core/DomainObjects/Email.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace NSE.Core.DomainObjects
+{
+    public class Email
+    {
+        public const int AddressMaxLength = 254;
+        public const int AddressMinLength = 5;
+        public string Address { get; private set; }
+
+        //Construtor do EntityFramework
+        protected Email() { }
+
+        public Email(string address)
+        {
+            if (!Validar(address)) throw new DomainException("E-mail inválido");
+            Address = address;
+        }
+
+        public static bool Validar(string email)
+        {
+            var regexEmail = new Regex(@"^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$");
+            return regexEmail.IsMatch(email);
+        }
+    }
+}
+
---
+++ src/building blocks/NSE.Core/Util/StringUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace NSE.Core.Util
+{
+    public static class StringUtils
+    {
+        public static string OnlyNumbers(this string str, string input)
+        {
+            return new string(input.Where(char.IsDigit).ToArray());
+        }
+    }
+}
+
